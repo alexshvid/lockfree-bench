d56057054deba4ac784ed7a33ac187fa480fb273
Alex Shvid <a@shvid.com>
2014-03-17 12:22:13 -0700

    Thread.sleep experiments

--- src/SpinLock.java
+++ src/SpinLock.java
@@ -7,6 +7,11 @@ public class SpinLock {
 	public void acquire() {
 		while (!locked.compareAndSet(false, true)) {
 			
+			//try {
+		//		Thread.sleep(0, 250000);
+		//	} catch (InterruptedException e) {
+		//	}
+			
 			JvmTools.usleep(250);
 		}
 	}
